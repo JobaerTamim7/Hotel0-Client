59db122840500103aea36ce60a9723cf080e1120
Tamim <jobaertamim7@gmail.com>
2025-07-29 23:01:42 +0600

    feat(home) add our services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import HomeMap from "../components/HomeMap";
 import HomeMapSideCard from "../components/HomeMapSideCard";
 import FAQSection from "../components/FAQ";
+import OurServices from "../components/OurServices";
 
 export default function Home() {
   document.title = "Hotel-0 Home";
@@ -14,6 +15,9 @@ export default function Home() {
       <div className="flex flex-col justify-around items-center md:flex-row px-4 md:px-10 mt-10">
         <HomeMap />
         <HomeMapSideCard />
+      </div>
+      <div className="px-10 mt-10">
+        <OurServices />
       </div>
       <div className="px-10 mt-10">
         <FAQSection />
